3cec73fe7e8d4dee5925715f617a09c2ab80796c
Nilam Jadhav <99525537+nilamsjadhav@users.noreply.github.com>
2022-05-30 14:36:33 +0530

    Extracted function from isPointInsideSquare

--- square.js
+++ square.js
@@ -4,27 +4,34 @@ class Square {
     this.side = side;
   }
 
-  diagonalEdge() {
+  diagonalEdge() { // bottomRightDiagonalEdge
     const x = this.origin.x + this.side;
     const y = this.origin.y - this.side;
     this.bottomRight = { x, y };
-    return this.bottomRight;
   }
 
   isNumberInRange(start, end, number) {
     return start < number < end;
   }
 
-  isPointInsideSquare(point) {
-    this.diagonalEdge();
+  isPointInRange(point) {
     const xCoOrdinates = [this.origin.x, this.bottomRight.x, point.x];
     const yCoOrdinates = [this.origin.y, this.bottomRight.y, point.y];
-    return this.isNumberInRange(...xCoOrdinates) && this.isNumberInRange(...yCoOrdinates);
+
+    return this.isNumberInRange(...xCoOrdinates)
+      && this.isNumberInRange(...yCoOrdinates);
+  }
+
+  isPointInsideSquare(point) {
+    this.diagonalEdge();
+    return this.isPointInRange(point);
   }
 }
 
 const origin = { x: 2, y: 4 };
-const sqr = new Square(origin, 3);
+const side = 3;
+
+const sqr = new Square(origin, side);
 
 let point = { x: 3, y: 9 };
 console.log(sqr.isPointInsideSquare(point)); // false
